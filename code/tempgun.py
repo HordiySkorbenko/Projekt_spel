4d987e99a251d8ff86261cd8004d9c3d31f8af50
Klim N <klim.nikitenko@elev.ga.dbgy.se>
2026-01-05 00:06:59 +0100

    gjorde klart gun class

---
+++ code/tempgun.py
@@ -0,0 +1,39 @@
+#todo lägg in dethär i sprites
+"""todo: i game setup 'entities' lägga till:
+self.gun = Gun(self.player, self.all_sprites)
+import math
+"""
+from settings import *
+from math import atan2,degrees
+
+class Gun(pygame.sprite.Sprite):
+    def __init__(self, player,groups):
+        # koppling till player
+        self.player = player
+        self.distance = 140
+        self.player_direction = pygame.Vector2(0,1)
+        #pistol sprite position
+        super().__init__(groups)
+        self.gun_surf = pygame.image.load(join('images','gun','gun.png')).convert_alpha()
+        self.image = self.gun_surf
+        self.rect= self.image.get_rect(center = self.player.rect.center + self.player_direction * self.distance)
+
+    def get_driection(self):
+        mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
+        player_pos = pygame.Vector2(WINDOW_WIDTH/2 ,WINDOW_HEIGHT/2)
+        self.player_direction =(mouse_pos-player_pos).normalize()
+
+    def rotate_gun(self):
+        angle = degrees(atan2(self.player_direction.x, self.player_direction.y)) - 90
+        if self.player_direction.x > 0:
+            self.image = pygame.transform.rotozoom(self.gun_surf,angle,1)
+        else:
+            self.image = pygame.transform.rotozoom(self.gun_surf,abs(angle),1)
+            self.image = pygame.transform.flip(self.image,False,True)
+
+
+    #pistolen flyttar med spelaren
+    def update(self, _):
+        self.get_direction()
+        self.rotate_gun()
+        self.rect.center = self.player.rect.center + self.player_direction * self.distance
