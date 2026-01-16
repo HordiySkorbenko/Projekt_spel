67a42760e88228b6e74b9f80c30ca7a519b9de89
HordiySkorbenko <hordiy.skrobenko@elev.ga.dbgy.se>
2026-01-16 10:53:20 +0100

    fixade Direction

--- code/sprites.py
+++ code/sprites.py
@@ -22,11 +22,11 @@ class Gun(pygame.sprite.Sprite):
         self.player_direction = pygame.Vector2(0,1)
         #pistol sprite position
         super().__init__(groups)
-        self.gun_surf = pygame.image.load(join('images','gun','gun.png')).convert_alpha()
+        self.gun_surf = pygame.image.load(join('image','gun','gun.png')).convert_alpha()
         self.image = self.gun_surf
         self.rect= self.image.get_rect(center = self.player.rect.center + self.player_direction * self.distance)
 
-    def get_driection(self):
+    def get_direction(self):
         mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
         player_pos = pygame.Vector2(WINDOW_WIDTH/2 ,WINDOW_HEIGHT/2)
         self.player_direction =(mouse_pos-player_pos).normalize()
